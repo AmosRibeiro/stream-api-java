4ede95f5a4f3a8a37b8913fdfdbddd4094a68830
Amós Ribeiro <amosribeiro2708@gmail.com>
2024-11-01 17:12:19 -0300

    uso da functional interface supplier

---
+++ src/SupplierExample.java
@@ -0,0 +1,17 @@
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.List;
+
+public class SupplierExample {
+    public static void main(String[] args) {
+        
+        Supplier<String> saudacao = () -> "Olá, seja bem vindo(a)!";
+
+        List<String> listaSaudacoes = Stream.generate(saudacao)
+            .limit(5)
+            .collect(Collectors.toList());
+
+        listaSaudacoes.forEach(s -> System.out.println(s));
+    }
+}
