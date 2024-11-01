44651f59a54b8e6813b6d6fe039c681c0f9064ec
Amós Ribeiro <amosribeiro2708@gmail.com>
2024-11-01 17:16:54 -0300

    method reference

--- src/SupplierExample.java
+++ src/SupplierExample.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class SupplierExample {
     public static void main(String[] args) {
-        
+
         Supplier<String> saudacao = () -> "Olá, seja bem vindo(a)!";
 
         List<String> listaSaudacoes = Stream.generate(saudacao)
             .limit(5)
             .collect(Collectors.toList());
 
-        listaSaudacoes.forEach(s -> System.out.println(s));
+        listaSaudacoes.forEach(System.out::println);
     }
 }
